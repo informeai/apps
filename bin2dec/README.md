b1a33f3b1f8e7bc018473406114f63eba20735cb
Wellington Gadelha <wellingtongadelha@Informeai.local>
2020-04-10 08:33:50 -0300

    Evaluate number finish.

--- bin2dec/README.md
+++ bin2dec/README.md
@@ -4,5 +4,5 @@ Transform binary for decimal.
 ## Tasks
 
 - [x] Create function for input numbers.
-- [ ] Evaluate the number type.
+- [x] Evaluate the number type.
 - [ ] Presentation of result to user. 
--- bin2dec/bin_to_dec.py
+++ bin2dec/bin_to_dec.py
@@ -12,11 +12,19 @@ print("""
 
 def convert_num(value:int):
     return int(value, 2)
+def evaluate_num(value):
+    for n in value:
+        if n is '0' or n is '1':
+            pass
+        else:
+            return True
 
 num_bin = input('Input the binary sequence: Ex: 00000001 >> ').strip()
 
 
 if len(num_bin) > 8:
     print('Number max of digits is 8.')
-elif not isinstance(num_bin, int):
-    print('Not number valid.')
+elif evaluate_num(num_bin):
+    print('Number format not compost of 0 or 1')
+else:
+    pass
